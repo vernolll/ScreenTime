275476a199c9fa049e657a0ea7f673e0fe5b589d
Марина Карась <marina2006.karas@yandex.ru>
2025-01-06 19:45:24 +0300

    init

---
+++ ScreenTime.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <json/json.h>  // Подключаем jsoncpp
+
+int main() {
+    // Создаем объект JSON
+    Json::Value root;
+    root["name"] = "ScreenTime";
+    root["version"] = "1.0";
+
+    // Выводим его
+    std::cout << root << std::endl;
+
+    return 0;
+}
---
+++ ScreenTime.h
@@ -0,0 +1,6 @@
+﻿#ifndef SCREEN_TIME_H
+#define SCREEN_TIME_H
+
+// Здесь могут быть объявления, если нужны
+
+#endif // SCREEN_TIME_H
